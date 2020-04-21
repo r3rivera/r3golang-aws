b6afc7e224011d4caf63a12faf4ef80d6e6c071d
Ryan Rivera <r.ryan.rivera@outlook.com>
2020-04-20 20:16:23 -0500

    Added login and verify flow

--- cognito/login-user.go
+++ cognito/login-user.go
@@ -1,1 +1,42 @@
 package cognito
+
+import (
+	"fmt"
+
+	aws "github.com/aws/aws-sdk-go/aws"
+	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
+)
+
+//LoginUser Authenticate the user
+func (c *UserCognitoClient) LoginUser(username, password *string) UserAuthToken {
+	fmt.Println("Authenticating user :: ", username)
+
+	m := make(map[string]*string)
+	m["USERNAME"] = username
+	m["PASSWORD"] = password
+
+	rqst := &cognito.AdminInitiateAuthInput{
+		AuthFlow:       aws.String(cognito.AuthFlowTypeAdminNoSrpAuth),
+		ClientId:       &c.AppClientID,
+		UserPoolId:     &c.UserPoolID,
+		AuthParameters: m,
+	}
+
+	processor, output := c.CognitoClient.AdminInitiateAuthRequest(rqst)
+	err := processor.Send()
+
+	if err != nil {
+		fmt.Println("Error authenticating the user! ", err)
+		panic(err)
+	}
+
+	if output != nil && output.AuthenticationResult != nil {
+		return UserAuthToken{
+			AccessToken:  *output.AuthenticationResult.AccessToken,
+			RefreshToken: *output.AuthenticationResult.RefreshToken,
+			Expiration:   *output.AuthenticationResult.ExpiresIn,
+		}
+	}
+
+	return UserAuthToken{}
+}
--- cognito/verify-email.go
+++ cognito/verify-email.go
@@ -9,7 +9,7 @@ import (
 
 //GenerateVerficationCodeForEmail generates code for email with the AWS AccessToken receieved during authentication
 func (c *UserCognitoClient) GenerateVerficationCodeForEmail(accessToken *string) bool {
-	fmt.Println("Verifying email address using accessToken:: ", accessToken)
+	fmt.Println("Generating code for verifying email address using accessToken :: ", &accessToken)
 
 	rqst := &cognito.GetUserAttributeVerificationCodeInput{
 		AccessToken:   accessToken,
@@ -21,6 +21,26 @@ func (c *UserCognitoClient) GenerateVerficationCodeForEmail(accessToken *string)
 
 	if err != nil {
 		fmt.Println("Error generating code for email verification!", err)
+		panic(err)
+	}
+	return output != nil
+}
+
+//VerifyEmailByCode verfies the email address using the accessToken
+func (c *UserCognitoClient) VerifyEmailByCode(accessToken, code *string) bool {
+	fmt.Println("Verifying email address using accessToken and code!")
+
+	rqst := &cognito.VerifyUserAttributeInput{
+		AccessToken:   accessToken,
+		AttributeName: aws.String("email"),
+		Code:          code,
+	}
+
+	processor, output := c.CognitoClient.VerifyUserAttributeRequest(rqst)
+	err := processor.Send()
+
+	if err != nil {
+		fmt.Println("Error verifying email using code!", err)
 		panic(err)
 	}
 	return output != nil
